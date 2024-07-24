a6ebca824fbd8d3223d233c4d6c42c76206a05e8
Tal <taltatal2@gmail.com>
2024-07-24 17:11:06 +0300

    cleanup

--- httpserver.py
+++ httpserver.py
@@ -1,8 +1,6 @@
 import argparse
 import datetime
-import multiprocessing
 import threading
-import time
 
 import cv2
 from flask import Flask, Response
@@ -10,60 +8,59 @@ from flask import render_template
 
 from src.main_face_recognition import wide_image_generator
 
-app = Flask(__name__, template_folder="./")
+class App(Flask):
+    def __init__(self, name, template_folder=None):
+        super().__init__(name, template_folder=template_folder)
+        self.current_frame = threading.Lock()
+        self.current_frame_lock = threading.Lock()
+        self.add_url_rules()
+        self.updater_thread = CurrentFrameUpdaterThread(self)
+
+    def add_url_rules(self):
+        self.add_url_rule("/", "index", self.index)
+        self.add_url_rule("/video_feed", "video_feed", self.video_feed)
+
+    def index(self):
+        return render_template("index.html")
+
+    def video_feed(self):
+        return Response(self.current_frame_generator(),
+                        mimetype="multipart/x-mixed-replace; boundary=frame")
+
+    def current_frame_generator(self):
+        while True:
+            with self.current_frame_lock:
+                if self.current_frame is None:
+                    continue
+
+                (flag, encodedImage) = cv2.imencode(".jpg", self.current_frame)
+                if not flag:
+                    continue
+            yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
+                   bytearray(encodedImage) + b'\r\n')
 
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+class CurrentFrameUpdaterThread(threading.Thread):
+    def __init__(self, app: App):
+        super().__init__(target=self.main_loop, daemon=True)
+        self.app = app
+        self.start()
 
+    def main_loop(self):
+        kill_flag = threading.Event()
 
-def detect_motion():
-    kill_flag = multiprocessing.Event()
-
-    global outputFrame, lock
-
-    for frame in wide_image_generator(kill_flag, "data/target_face_images/", "weights/yolov8n-face.trt",
-                                      "weights/arcfaceresnet100-8.trt", 0.65, 0.5, 0.5):
-        timestamp = datetime.datetime.now()
-        cv2.putText(frame, timestamp.strftime(
-            "%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
-        with lock:
-            outputFrame = frame.copy()
-
-
-def generate():
-    global outputFrame, lock
-    while True:
-        with lock:
-            if outputFrame is None:
-                continue
-
-            outputFrame = cv2.resize(outputFrame, (800, 800))
-            (flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
-            if not flag:
-                continue
-        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
-               bytearray(encodedImage) + b'\r\n')
-
-
-@app.route("/video_feed")
-def video_feed():
-    return Response(generate(),
-                    mimetype="multipart/x-mixed-replace; boundary=frame")
+        for frame in wide_image_generator(kill_flag, "data/target_face_images/", "weights/yolov8n-face.trt",
+                                          "weights/arcfaceresnet100-8.trt", 0.65, 0.5, 0.5):
+            timestamp = datetime.datetime.now()
+            cv2.putText(frame, timestamp.strftime(
+                "%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+            with self.app.current_frame_lock:
+                self.app.current_frame = frame.copy()
 
 
 def main(ip, port):
-    global outputFrame, lock
-    outputFrame = None
-    lock = threading.Lock()
-
-    time.sleep(2.0)
-
-    t = threading.Thread(target=detect_motion)
-    t.daemon = True
-    t.start()
+    app = App(__name__, template_folder="./")
     app.run(host=ip, port=port, debug=True,
             threaded=True, use_reloader=False)
 
@@ -71,10 +68,8 @@ def main(ip, port):
 # check to see if this is the main thread of execution
 if __name__ == '__main__':
     # construct the argument parser and parse command line arguments
-    ap = argparse.ArgumentParser()
-    ap.add_argument("--ip", type=str, required=True,
-                    help="ip address of the device")
-    ap.add_argument("--port", type=int, required=True,
-                    help="ephemeral port number of the server (1024 to 65535)")
-    args = ap.parse_args()
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument("--ip", type=str, required=True)
+    args_parser.add_argument("--port", type=int, required=True)
+    args = args_parser.parse_args()
     main(args.ip, args.port)
